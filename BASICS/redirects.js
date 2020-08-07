dd5062b1c6183fd76e2af08a5470f59785bc3739
adithyaanilkumar <adithyaanilkumar1@gmail.com>
2020-08-07 18:26:27 +0530

    added redirect

--- BASICS/redirects.js
+++ BASICS/redirects.js
@@ -1,14 +1,18 @@
 const http = require('http'); // importing or requiring http module
+const fs = require('fs')
 
 const server = http.createServer((req,res)=>{ //creating a server listener for requests
-    const url = req.url // takinf the url  from the request obj
+    const url = req.url; // takinf the url  from the request obj
+    const method = req.method;// the method of the request, ie GET, POST etc
     if( url === '/'){
         res.setHeader('Content-Type','text/html'); // setting the response headers to html so that the response can be a html respose
         res.write('<html><body><form action="/message" method="POST"><input type="text" name ="message"><button type = "submit">submit</button></form></body></html>'); //writing the form ofr submission
         res.end(); // ending the response so that it can be sent
         return res.end(); //returning after ending the response as it shouldnt excecute anny other response 
     }
-    //the below  is printed because the route is /message and not / so the if condition is excecuted only once 
+    if(url === '/message' && method ==='POST'){
+        fs.writeFileSync('mesage.txt','DUMMY');
+    }
     console.log("in server");
     console.log(res); // prints the response object
     res.setHeader('Content-Type','text/html'); // setting the response headers to html so that the response can be a html respose
