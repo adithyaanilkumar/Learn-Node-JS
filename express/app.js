1db6c5b7c89bffa66de0b6fcce26ab23b7123f69
adithyaanilkumar <adithyaanilkumar1@gmail.com>
2020-08-18 20:33:32 +0530

    added express uses

--- express/app.js
+++ express/app.js
@@ -2,7 +2,8 @@ const http = require('http'); // importing or requiring http module
 const express = require('express');
 const app = express();
 app.use((req,res,next)=>{
-    
+    console.log("In mw")
+    res.send()
 })
 const server = http.createServer(app);
 server.listen(8080); // listening to the requests 
