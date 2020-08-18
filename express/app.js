adb95608952d82b523c7ece1fae5612d5e5dc661
adithyaanilkumar <adithyaanilkumar1@gmail.com>
2020-08-18 22:16:18 +0530

    add app.use

--- express/app.js
+++ express/app.js
@@ -1,9 +1,12 @@
 const http = require('http'); // importing or requiring http module
 const express = require('express');
 const app = express();
-app.use((req,res,next)=>{
-    console.log("In mw")
-    res.send()
+app.use('/route',(req,res,next)=>{
+    console.log("In rioute")
+    res.send("<h1>hello rotued</h1>");
 })
-const server = http.createServer(app);
-server.listen(8080); // listening to the requests 
+app.use('/',(req,res,next)=>{
+    console.log("In not route")
+    res.send("<h1>hello normal</h1>");
+})
+app.listen(8080); // listening to the requests 
