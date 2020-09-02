3880410b3ded1a56d4a3da211a6b73aa532190a1
adithyaanilkumar <adithyaanilkumar1@gmail.com>
2020-09-02 18:03:03 +0530

    added html serving 404

--- express/app.js
+++ express/app.js
@@ -1,6 +1,7 @@
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser')
 
 const app = express();
@@ -8,6 +9,6 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 app.use((req,res,next)=>{
-    res.status(404).send("<h1> 404 not found</h1>");
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 });
 app.listen(8080); // listening to the requests 
--- express/routes/admin.js
+++ express/routes/admin.js
@@ -1,11 +1,13 @@
 const express = require('express')
+const path = require('path')
 const router = express.Router()
+
 router.get('/product',(req,res,next)=>{
-    res.send('<form action="/admin/product" method="POST"><input type="text" name ="message"><button type = "submit">submit</button></form>');
-})
+    res.sendFile(path.join(__dirname,'../','views','add-product.html'));
+});
 router.post('/product',(req,res,next)=>{
     console.log("In prod");
     console.log(req.body);
     res.redirect('/admin/product');
-})
+});
 module.exports = router
