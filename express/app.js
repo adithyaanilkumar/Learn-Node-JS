68ff4be47711ea26d3e0575a379469ed399cfdb0
adithyaanilkumar <adithyaanilkumar1@gmail.com>
2020-09-03 16:20:30 +0530

    added static

--- express/app.js
+++ express/app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser')
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:false}))
+app.use(express.static(path.join(__dirname,'public')));
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 app.use((req,res,next)=>{
