4d53caca8785f111a4915d5ad632479803c1354b
adithyaanilkumar <adithyaanilkumar1@gmail.com>
2020-09-03 21:30:18 +0530

    added data hadling

--- express/app.js
+++ express/app.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser')
 const app = express();
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(express.static(path.join(__dirname,'public')));
-app.use('/admin',adminRoutes);
+app.use('/admin',adminRoutes.router);
 app.use(shopRoutes);
 app.use((req,res,next)=>{
     res.status(404).sendFile(path.join(__dirname,'views','404.html'));
--- express/routes/admin.js
+++ express/routes/admin.js
@@ -2,13 +2,16 @@ const express = require('express')
 const path = require('path')
 const rootDir = require('../utils/homedir')
 const router = express.Router()
-
-router.get('/product',(req,res,next)=>{
+var product = []
+router.get('/add-product',(req,res,next)=>{
     res.sendFile(path.join(rootDir,'views','add-product.html'));
 });
-router.post('/product',(req,res,next)=>{
+router.post('/add-product',(req,res,next)=>{
     console.log("In prod");
-    console.log(req.body);
-    res.redirect('/admin/product');
+    console.log("logging input"+req.body.title);
+    product.push(req.body.title);
+    console.log("product",product);
+    res.redirect('/admin/add-product');
 });
-module.exports = router
+exports.router = router
+exports.product = product
